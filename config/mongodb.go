d8337110bfd5ae3b690c39d8f14a3f89f9bb02c5
Muflih Kholidin <muflic.24@gmail.com>
2020-01-01 06:58:15 +0700

    Update mongodb config

--- config/mongodb.go
+++ config/mongodb.go
@@ -3,7 +3,6 @@ package config
 import (
 	"github.com/globalsign/mgo"
 	"github.com/mmuflih/envgo/conf"
-	"log"
 )
 
 /**
@@ -15,9 +14,15 @@ import (
 
 func NewMongoDB(cfg conf.Config) (error, *mgo.Database) {
 	address := cfg.GetString(`mongodb.address`)
+	user := cfg.GetString(`mongodb.user`)
+	pass := cfg.GetString(`mongodb.pass`)
 	database := cfg.GetString(`mongodb.database`)
-	log.Println("mongo config", address, database)
-	session, err := mgo.Dial(address)
-	session.SetMode(mgo.Monotonic, true)
-	return err, session.DB(database)
+	port := cfg.GetString(`mongodb.port`)
+	session, err := mgo.Dial(address + ":" + port)
+	dbSession := session.DB(database)
+	auth := cfg.GetBool(`mongodb.auth`)
+	if auth {
+		dbSession.Login(user, pass)
+	}
+	return err, dbSession
 }
